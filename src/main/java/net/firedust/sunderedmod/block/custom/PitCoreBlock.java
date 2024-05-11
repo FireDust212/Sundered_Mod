e18a132396d21cf2f3ba8cf92a52148440e5b35a
FireDust212 <firedust212@gmail.com>
2024-05-11 00:02:45 -0400

    Pit core spreading and eating working. Not really following tutorial anymore.

--- src/main/java/net/firedust/sunderedmod/block/custom/PitCoreBlock.java
+++ src/main/java/net/firedust/sunderedmod/block/custom/PitCoreBlock.java
@@ -2,11 +2,14 @@ package net.firedust.sunderedmod.block.custom;
 
 import net.firedust.sunderedmod.block.entity.ModBlockEntities;
 import net.firedust.sunderedmod.block.entity.PitCoreBlockEntity;
+import net.firedust.sunderedmod.entity.custom.PitCreatureEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.util.valueproviders.IntProvider;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
@@ -28,6 +31,21 @@ public class PitCoreBlock extends SunderedSpreaderBlock<PitCoreBlockEntity> {
     public int getExpDrop(BlockState state, LevelReader level, RandomSource randomSource, BlockPos pos, int fortuneLevel, int silkTouchLevel) {
         return silkTouchLevel == 0 ? this.xpRange.sample(randomSource) : 0;
     }
+
+    // Same stepOn method as magma block, but without the sneaking and frostwalker check
+    @Override
+    public void stepOn(Level pLevel, BlockPos pPos, BlockState pState, Entity pEntity) {
+        if (!(pEntity instanceof PitCreatureEntity))
+            pEntity.hurt(pLevel.damageSources().hotFloor(), 1.0F);
+        super.stepOn(pLevel, pPos, pState, pEntity);
+    }
+
+    // Same fallOn method as pointed dripstone
+    @Override
+    public void fallOn(Level pLevel, BlockState pState, BlockPos pPos, Entity pEntity, float pFallDistance) {
+        pEntity.causeFallDamage(pFallDistance + 2.0F, 2.0F, pLevel.damageSources().stalagmite());
+    }
+
 
     @Nullable
     @Override
--- src/main/java/net/firedust/sunderedmod/block/entity/PitCoreBlockEntity.java
+++ src/main/java/net/firedust/sunderedmod/block/entity/PitCoreBlockEntity.java
@@ -1,21 +1,33 @@
 package net.firedust.sunderedmod.block.entity;
 
+import net.firedust.sunderedmod.entity.custom.PitCreatureEntity;
 import net.firedust.sunderedmod.util.ModTags;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.SculkSpreader;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.BlockPositionSource;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.level.gameevent.GameEventListener;
+import net.minecraft.world.level.gameevent.PositionSource;
+import net.minecraft.world.phys.Vec3;
 
 import java.util.Random;
 
-public class PitCoreBlockEntity extends SunderedSpreaderBlockEntity {
+public class PitCoreBlockEntity extends SunderedSpreaderBlockEntity implements GameEventListener.Holder{
     private int size;
     private int consumed;
+    private PitCoreListener pitCoreListener;
 
     public PitCoreBlockEntity(BlockPos pPos, BlockState pBlockState) {
         super(ModBlockEntities.PIT_CORE_BE.get(), pPos, pBlockState);
         this.size = 0;
-        this.consumed = 5;
+        this.consumed = 0;
+        this.pitCoreListener = new PitCoreListener(pBlockState, new BlockPositionSource(pPos), this.size + 1);
     }
 
     @Override
@@ -30,12 +42,13 @@ public class PitCoreBlockEntity extends SunderedSpreaderBlockEntity {
         super.load(pTag);
         size = pTag.getInt("pit_core_block.size");
         consumed = pTag.getInt("pit_core_block.consumed");
+        this.pitCoreListener.radius =  this.size + 1;
     }
 
     @Override
     protected boolean canSpreadDown(Level pLevel, BlockPos pPos, BlockState pState) {
         // Spread if pit has consumed enough
-        if (new Random().nextInt(20) == 0 && this.consumed >= this.size){
+        if (new Random().nextInt(20*(this.size+1)) == 0 && this.consumed >= this.size){
             // Get block below pit
             BlockState state = pLevel.getBlockState(new BlockPos(pPos.getX(), pPos.getY() - 1, pPos.getZ()));
 
@@ -54,11 +67,64 @@ public class PitCoreBlockEntity extends SunderedSpreaderBlockEntity {
 
         PitCoreBlockEntity newCore = new PitCoreBlockEntity(pos, pState);
         newCore.size = this.size + 1;
-        newCore.consumed = this.consumed - this.size;
+        newCore.consumed = this.consumed - newCore.size;
+
+        this.pitCoreListener = new PitCoreListener(newCore.getBlockState(), new BlockPositionSource(pos), this.size + 1);
 
         pLevel.setBlock(pos, pState, 3);
         pLevel.setBlockEntity(newCore);
 
         pLevel.destroyBlock(pPos, false);
     }
+
+    public void grow(){
+        ++this.consumed;
+    }
+
+    @Override
+    public PitCoreListener getListener() {
+        return this.pitCoreListener;
+    }
+
+    // Taking this from SculkCatlystBlockEntity
+    public static class PitCoreListener implements GameEventListener {
+        public int radius;
+        private final BlockState blockState;
+        private final PositionSource positionSource;
+
+        public PitCoreListener(BlockState pBlockState, PositionSource pPositionSource, int radius) {
+            this.blockState = pBlockState;
+            this.positionSource = pPositionSource;
+            this.radius = radius;
+        }
+
+        @Override
+        public PositionSource getListenerSource() {
+            return this.positionSource;
+        }
+
+        @Override
+        public int getListenerRadius() {
+            return this.radius;
+        }
+
+        @Override
+        public boolean handleGameEvent(ServerLevel serverLevel, GameEvent gameEvent, GameEvent.Context context, Vec3 pos) {
+            if (gameEvent == GameEvent.ENTITY_DIE && pos.y > this.positionSource.getPosition(serverLevel).get().y) {
+                Entity source = context.sourceEntity();
+                if (source instanceof LivingEntity && !(source instanceof PitCreatureEntity)) {
+                    LivingEntity livSource = (LivingEntity) source;
+                    if (!livSource.wasExperienceConsumed()) {
+                        livSource.skipDropExperience();
+                    }
+                    Vec3 pos2 = positionSource.getPosition(serverLevel).get();
+                    PitCoreBlockEntity pE = ((PitCoreBlockEntity) serverLevel.getBlockEntity(new BlockPos((int) pos2.x, (int) pos2.y, (int) pos2.z)));
+                    if(pE != null) pE.grow();
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
 }
+
